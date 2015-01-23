3c8b4194aaffea7c8025bacf1a0a568fcee375e4
Dane Worsham <zenki14@gmail.com>
2015-01-22 21:56:38 -0500

    restart cpu commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,6 @@
-var itemGrid, itemImage, itemTitle, itemShop, itemPrice, itemCurrency;
+var itemContainer, itemImage, itemTitle, itemShop, itemPrice, itemCurrency, daftListing, hoverImg;
 
-// , daftListing, hoverImg;
-
-itemContainer = $('.itemGrid')
+itemContainer = $(".itemGrid");
 
 etsyItems.results.forEach(function (item) {
 
@@ -12,19 +10,19 @@ etsyItems.results.forEach(function (item) {
       itemImage = "<img class='productImgs' src='" + pics.url_fullxfull + "' />'";
     });
 
-  hover_img = "<div class='foodBox'><img class='heart' src='https://raw.githubusercontent.com/tiy-atlanta-js/Assignments/master/Assignment%2008/assets/heart.png' />" + "<img class='hamburger' src='https://raw.githubusercontent.com/tiy-atlanta-js/Assignments/master/Assignment%2008/assets/hamburger.png' /></div>";
+  hoverImg = "<div class='itemBox'><img class='heart' src='https://raw.githubusercontent.com/tiy-atlanta-js/Assignments/master/Assignment%2008/assets/heart.png' />" + "<img class='hamburger' src='https://raw.githubusercontent.com/tiy-atlanta-js/Assignments/master/Assignment%2008/assets/hamburger.png' /></div>";
 
   itemShop = "<span class='shopTitle'>" + item.Shop.shop_name + "</span>";
 
   itemPrice = "<p class='isRight'>" + item.price + "</p>";
 
 
-  itemCurrency = "<p class='isRight'>" + item.currency_code + "</p>";
+  itemCurrency = "<p class='isRight'>"  + item.currency_code + "</p>";
 
   // Build Each Listing
-  daft_listing = "<li class='mainProducts'>" + itemImage + hoverImg + itemTitle + itemShop + itemCurrency + itemPrice +  "</li>";
+  daftListing = "<li class='mainProducts'>" + itemImage + hoverImg + itemTitle + itemShop + itemCurrency + itemPrice +  "</li>";
 
   // Append Each Item to The Grid
-  itemGrid.append(daftListing);
+  itemContainer.append(daftListing);
 
 });
