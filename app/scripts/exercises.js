b71561d93ddb6310f9700b1c6f7fd57a67db4b9c
Dane Worsham <zenki14@gmail.com>
2015-01-25 22:17:37 -0500

    ex2 complete

---
+++ app/scripts/exercises.js
@@ -0,0 +1,96 @@
+/**
+ * PART 1
+ *
+ * Write a JavaScript program that takes two numbers as parameters and computes the sum of those two numbers
+ *
+ * For example, if the user input 3 and 6, then the answer would be: "9 (3+6)".
+ */
+
+var sum = function(a, b) {
+    var sum = a + b;
+    return String(sum + ' (' + a + '+' + b + ')')
+}
+
+console.log(sum(8, 11));
+
+/**
+ * PART 2
+ *
+ * Write a JavaScript program that calculates the average time for this marathoner to complete the full marathon (26.2mi)
+ */
+
+var runner1  = {
+    finishingTime1: 240, //in minutes
+    finishingTime2: 210.4,
+    finishingTime3: 235.1,
+    finishingTime4: 208.9,
+    finishingTime5: 197.5
+};
+
+var runner2 = {
+    finishingTime1: 120, //in minutes
+    finishingTime2: 110.4,
+    finishingTime3: 135.1,
+    finishingTime4: 108.9,
+    finishingTime5: 97.5
+};
+
+var getAverageTime = function(person) {
+    // ... return ...
+    var key,
+        sum = 0,
+        count = 0;
+    for (key in person) {
+        sum += person[key];
+        count++;
+    }
+    return Math.round(sum / count * 10) / 10;
+}
+
+console.log(getAverageTime(runner1));
+console.log(getAverageTime(runner2));
+}
+
+
+
+/**
+ * PART 3
+ *
+ *
+ * Modify the following to calculate the difference between the two accounts.
+ */
+
+/**
+ * declarations
+ */
+
+var TimsBankAccount = {
+    checking: 0,
+    savings: 0,
+    retirement: 0
+};
+
+var RobertosBankAccount = {
+    checking: 0,
+    savings: 0,
+    retirement: 0
+};
+
+function addToBank(account, savings, retirement, checking) {
+    account.savings = account.savings + savings;
+    account.retirement = account.retirement + retirement;
+    account.checking = account.checking + checking;
+}
+
+function getSumOfAccounts(account) {
+    // return ...
+    return account.checking + account.savings + account.retirement;
+}
+
+addToBank(TimsBankAccount, 100, 10, 1);
+addToBank(RobertosBankAccount, 200, 50, 19);
+
+var sumOfTims = getSumOfAccounts(TimsBankAccount);
+var sumOfRobertos = getSumOfAccounts(RobertosBankAccount);
+
+console.log(sumOfTims - sumOfRobertos); // should calculate to -158
